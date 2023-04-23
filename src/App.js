d08610d4636ec55e76ac2b8fe01aeeaf21bad7ae
JafarDarwesh <73017113+JafarDarwesh@users.noreply.github.com>
2023-04-23 11:17:10 +0300

    Delete src directory

--- src/App.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import main from './main';
-
-export default class App extends main(){}
--- src/task1 Questions/Qs.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function Qs({k}) {
-  const qu=['HTML & CSS is for','React is framework for','In React we use *render* in','In React we use *Hooks* in',"FINISH"]
-  if (k<5) {
-    return <p>{qu[k]}</p>
-  }
-}
